ef83cde27a2e51b02f8857e106a9d4755a3ea5c3
arafath <marafath07@gmail.com>
2020-03-23 14:08:28 +0530

    add the bill caluclator
    added a bill calculator using javacript and jsquery inlcuding html and css

---
+++ 3_Calculate_Bills/script.js
@@ -0,0 +1,18 @@
+function calculate () {
+  var amount = $ ('#amount').val ();
+  var percentage = $ ('#percentage').val ();
+  var tip = amount * (percentage / 100);
+  var total = Number(amount) + tip;
+
+  // $  indicated the jQuery library
+  // Number () will pass the argument of the amount
+  // we are converting the string to Number
+
+  $ ('#tip').val (tip.toFixed (2));
+  $ ('#total').val (total.toFixed (2));
+  return false; // return false tell that not to read on the new page // it tells the browser not to load a new page
+  
+}
+    $ ('#calculator').submit(calculate);
+
+}
